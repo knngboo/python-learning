fdd4daa448be06cbc9ea47ac97c58deab32db961
orlando <216954379+knngboo@users.noreply.github.com>
2026-05-20 12:15:19 -0500

    definition version

--- p3-brazilian-real-to-usd.py
+++ p3-brazilian-real-to-usd.py
@@ -1,4 +1,7 @@
-user_cash = float(input("Enter the amount in Brazilian Real: "))
-exchange_rate = 1 / 5.03
-usd_amount = user_cash * exchange_rate
-print(f"The equivalent amount in USD is: {usd_amount}")
+def calculate_usd(real_amount):
+    exchange_rate = 1 / 5.03
+    return real_amount * exchange_rate
+
+
+user_real = float(input("Enter the amount in Brazilian Real: "))
+print(f"The equivalent amount in USD is: {calculate_usd(user_real)}")
