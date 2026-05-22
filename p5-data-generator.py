b60145c29f8a05ec5aa6c74ddf92b1b4de666cf7
orlando <216954379+knngboo@users.noreply.github.com>
2026-05-22 14:33:44 -0500

    refactored variable names and added explanations

--- p5-data-generator.py
+++ p5-data-generator.py
@@ -1,10 +1,14 @@
 import pandas as pd
 
 # Create a sample DataFrame
+# var = {"key1": ["value1"],
+#        "key2": ["value1", "value2"],
+#        "key3": ["value1", "value2", "value3"]}
 data = {
     "Name": ["Alice", "Bob", "Charlie", "David"],
     "Age": [25, 30, 35, 40],
     "City": ["New York", "Los Angeles", "Chicago", "Houston"],
 }
-df = pd.DataFrame(data)
-print(df)
+#
+df_data = pd.DataFrame(data)
+print(df_data)
